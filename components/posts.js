80d66cf37d1641b7541c2a5783aec7bb7f943119
Judith Arrizza <judith.arrizza@gmail.com>
2024-12-09 11:03:59 -0800

    completed blog functions

--- components/posts.js
+++ components/posts.js
@@ -1,12 +1,19 @@
+"use client";
+
+import { useOptimistic } from 'react';
 import { formatDate } from '@/lib/format';
 import LikeButton from './like-icon';
+import { togglePostLikeStatusFunc } from '@/actions/posts';
 
-function Post({ post }) {
+// https://console.cloudinary.com/console/c-a5d2862dad603f6cc1d9a8f3ea8b29/media_library/search?q=%7B%22searchByFolders%22%3A%5B%22nextjs-blogposts-mutations%22%5D%7D&view_mode=mosaic
+
+
+function Post({ post, action }) {
   return (
     <article className="post">
-      <div className="post-image">
+      {post.image && <div className="post-image">
         <img src={post.image} alt={post.title} />
-      </div>
+      </div>}
       <div className="post-content">
         <header>
           <div>
@@ -19,7 +26,12 @@ function Post({ post }) {
             </p>
           </div>
           <div>
-            <LikeButton />
+            <form 
+              action={action.bind(null, post.id)} 
+              className={post.isLiked ? 'liked' : ''}
+              >
+              <LikeButton />
+            </form>
           </div>
         </header>
         <p>{post.content}</p>
@@ -29,15 +41,38 @@ function Post({ post }) {
 }
 
 export default function Posts({ posts }) {
-  if (!posts || posts.length === 0) {
+
+  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(posts, (prevPosts, updatedPostId) => {
+    const updatedPostIndex = prevPosts.findIndex((post) => post.id === updatedPostId); 
+    if (updatedPostIndex === -1) {
+      return prevPosts;
+    } 
+    const updatedPost = { ...prevPosts[updatedPostIndex] };
+    updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+    updatedPost.isLiked = !updatedPost.isLiked;
+    const newPosts = [...prevPosts];
+    newPosts[updatedPostIndex] = updatedPost;
+    return newPosts;
+  });
+
+  if (!optimisticPosts || optimisticPosts.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
+  }
+
+  async function updatePost(postId) {
+    updateOptimisticPosts(postId);
+    try {
+      await togglePostLikeStatusFunc(postId);
+    } catch (error) {
+      throw new Error('Failed on call to toggle Like Status');
+    }
   }
 
   return (
     <ul className="posts">
-      {posts.map((post) => (
+      {optimisticPosts.map((post) => (
         <li key={post.id}>
-          <Post post={post} />
+          <Post post={post} action={updatePost} />
         </li>
       ))}
     </ul>
